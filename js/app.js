db46f137651f1949eb3b4ee1142427f2362a0398
Stu <stuart@blueprintinteractive.com>
2015-06-12 17:37:17 -0400

    add button styles

--- js/app.js
+++ js/app.js
@@ -1,22 +1,13 @@
 angular.module('myApp', [])
 	.controller('MyCtrl', function($scope) {
-		$scope.name = 'Name';
-		$scope.jobTitle = "Job Title";
-		$scope.tediousTask = "Tedious Task";
-		$scope.dirtyTask = "Dirty Task";
-		$scope.celebrity = "Celebrity";
-		$scope.uselessSkill = "Useless Skill";
-		$scope.obnoxiousCelebrity = "Obnoxious Celebrity";
-		$scope.hugeNumber = "Huge Number";
-		$scope.adjective = "adjective";
 		$scope.gender = "Male";
+		$scope.activeView = 'form';
 
 		$scope.reset = function() {
 			$scope.data = '';
 			$scope.activeView = 'form';
 		};
 
-		$scope.activeView = 'form';
 
 		$scope.changeGender = function() {
 			if ($scope.gender == "Male") {
